cab8e51e6ec8457a4014979f6ac8eb8e87af40c2
NIRAJ <nirajvg1@gmail.com>
2021-12-08 22:03:32 +0530

    Home page with navigation added

--- app/src/main/java/xyz/codewithcoffee/cyc_app/Timetable.java
+++ app/src/main/java/xyz/codewithcoffee/cyc_app/Timetable.java
@@ -2,7 +2,9 @@ package xyz.codewithcoffee.cyc_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Timetable extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Timetable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
+    }
+
+    public void goto_home_page_navigation(View view) {
+        Intent intent=new Intent(view.getContext(),home_page_navigation.class);
+        startActivity(intent);
     }
 }
